37026c6d13b13d296013b02c25f5f224b150cb6b
Fengtan <Fengtan@847318.no-reply.drupal.org>
2015-05-23 22:23:01 -0400

    Add UI to select code review type.

--- src/main/java/org/jenkinsci/plugins/drupal/DrupalInstanceBuilder.java
+++ src/main/java/org/jenkinsci/plugins/drupal/DrupalInstanceBuilder.java
@@ -51,14 +51,7 @@ public class DrupalInstanceBuilder extends Builder {
 
     @Override
     public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
-        // This is where you 'build' the project.
-        // Since this is a dummy, we just say 'hello world' and call that a build.
-
-        // This also shows how you can consult the global configuration of the builder
-        if (getDescriptor().getUseFrench())
-            listener.getLogger().println("Bonjour, "+name+"!");
-        else
-            listener.getLogger().println("Hello, "+name+"!");
+        listener.getLogger().println("Hello, "+name+"!");
         return true;
     }
 
@@ -80,15 +73,6 @@ public class DrupalInstanceBuilder extends Builder {
      */
     @Extension // This indicates to Jenkins that this is an implementation of an extension point.
     public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
-        /**
-         * To persist global configuration information,
-         * simply store it in a field and call save().
-         *
-         * <p>
-         * If you don't want fields to be persisted, use <tt>transient</tt>.
-         */
-        private boolean useFrench;
-
         /**
          * In order to load the persisted global configuration, you have to 
          * call load() in the constructor.
@@ -126,28 +110,13 @@ public class DrupalInstanceBuilder extends Builder {
          * This human readable name is used in the configuration screen.
          */
         public String getDisplayName() {
-            return "Make a Drupal instance";
+            return "Build a Drupal instance";
         }
 
         @Override
         public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
-            // To persist global configuration information,
-            // set that to properties and call save().
-            useFrench = formData.getBoolean("useFrench");
-            // ^Can also use req.bindJSON(this, formData);
-            //  (easier when there are many fields; need set* methods for this, like setUseFrench)
             save();
             return super.configure(req,formData);
-        }
-
-        /**
-         * This method returns true if the global configuration says we should speak French.
-         *
-         * The method name is bit awkward because global.jelly calls this method to determine
-         * the initial state of the checkbox by the naming convention.
-         */
-        public boolean getUseFrench() {
-            return useFrench;
         }
     }
 }
