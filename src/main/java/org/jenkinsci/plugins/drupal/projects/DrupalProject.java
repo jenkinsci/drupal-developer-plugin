41537e1c1b520ce2258466d07339db77ec0f2379
Fengtan <Fengtan@847318.no-reply.drupal.org>
2015-07-24 15:19:51 -0400

    Add project desription.

--- src/main/java/org/jenkinsci/plugins/drupal/projects/DrupalProject.java
+++ src/main/java/org/jenkinsci/plugins/drupal/projects/DrupalProject.java
@@ -3,19 +3,16 @@ package org.jenkinsci.plugins.drupal.projects;
 import hudson.Extension;
 import hudson.model.ItemGroup;
 import hudson.model.TopLevelItem;
+import hudson.model.AbstractProject.AbstractProjectDescriptor;
 import hudson.model.Project;
 import hudson.plugins.checkstyle.CheckStylePublisher;
-import hudson.tasks.Builder;
 import hudson.tasks.junit.JUnitResultArchiver;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import jenkins.model.Jenkins;
 
 import org.jenkinsci.plugins.drupal.builders.CoderReviewBuilder;
 import org.jenkinsci.plugins.drupal.builders.DrupalInstanceBuilder;
 import org.jenkinsci.plugins.drupal.builders.SimpletestBuilder;
+import org.jenkinsci.plugins.drupal.scm.DrushMakefileSCM;
 
 /**
  * Drupal project (top level item).
@@ -27,12 +24,20 @@ public class DrupalProject extends Project<DrupalProject, DrupalBuild> implement
 
 	public DrupalProject(ItemGroup parent, String name) {
 		super(parent, name);
+
+		// Add SCM.
+		// getSCMs().add(new DrushMakefileSCM("api=2&#xD;core=7.x&#xD;projects[drupal][version]=7.38", "drupal")); // TODO GitSCM ?
+		
+		// Add builders.
+		getBuildersList().add(new DrupalInstanceBuilder("mysql://user:password@localhost/db", "drupal", "standard", false, false));
+		getBuildersList().add(new CoderReviewBuilder(true, true, true, true, true, "drupal", "logs.coder", "", false));
+		getBuildersList().add(new SimpletestBuilder("http://localhost/", "drupal", "logs.simpletest"));
+		
+		// Add publishers.
 		getPublishersList().add(new CheckStylePublisher("", "", "low", "", false, "", "", "0", "", "", "", "", "", "", "0", "", "", "", "", "", "", false, false, false, false, false, "logs.coder/*"));
 		getPublishersList().add(new JUnitResultArchiver("logs.simpletest/*"));
-		// TODO test unstable/fail thresholds
-		// TODO move into this.getPublishersList() { super() }
-		// TODO analysis collector ?
 		// TODO make sure dependency shows up in UI when installing module
+		// TODO project settings are not persisted
 	}
 	
 	@Override
@@ -44,16 +49,6 @@ public class DrupalProject extends Project<DrupalProject, DrupalBuild> implement
 		return (DescriptorImpl) Jenkins.getInstance().getDescriptorOrDie(getClass());
 	}
 	
-	@Override
-	public List<Builder> getBuilders() {
-		List<Builder> builders = new ArrayList<Builder>();
-		builders.add(new DrupalInstanceBuilder("mysql://user:password@localhost/db", "drupal", "standard", false, false));
-		builders.add(new CoderReviewBuilder(true, true, true, true, true, "drupal", "logs.coder", "", false)); // TODO profiles/** ?
-		builders.add(new SimpletestBuilder("http://localhost/", "drupal", "logs.simpletest"));
-		return builders;
-		// TODO add Makefile / git checkout drupal
-	}
-	
 	@Extension
 	public static final class DescriptorImpl extends AbstractProjectDescriptor {
 		
@@ -63,7 +58,6 @@ public class DrupalProject extends Project<DrupalProject, DrupalBuild> implement
 		public String getDisplayName() {
 			return "Drupal project";
 		}
-		// TODO description does not seem to show up
 		
 		@Override
 		public DrupalProject newInstance(ItemGroup parent, String name) {
